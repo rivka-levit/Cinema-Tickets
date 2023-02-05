51b3c74767fa9736a26b93c2fbf6d914980940f8
rivka_yuliia <juliacreadora@gmail.com>
2023-02-06 00:30:12 +0200

    Implement Seat, User, Card classes. Create interface

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-from purchase import Seat
+from purchase import User, Seat, Card
 
+user = User(input('Your full name: '))
+seat = Seat(input('Preferred seat number: '))
+if not seat.is_free():
+    print('Seat is taken!')
+else:
+    card_type = input('Your card type: ')
+    card_number = int(input('Your card number: '))
+    card_cvc = int(input('Your card cvc: '))
+    card_holder = input('Card holder name: ')
+
+    card = Card(card_type, card_number, card_cvc, card_holder)
+    if not card.validate(seat.price):
+        print("There's a problem with your card")
+    else:
+        user.buy(seat, card)
+        print('Purchase successful!')
--- purchase.py
+++ purchase.py
@@ -17,15 +17,27 @@ class Seat:
         connection.close()
         return float(*res[0])
 
-    def is_free(self, seat_id: str) -> bool:
-        pass
+    def is_free(self) -> bool:
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+                        SELECT "taken" FROM 'Seat' WHERE "seat_id" == ?
+                        """, [self.seat_id])
+        res = cursor.fetchall()
+        connection.close()
+        return res[0][0] == 0
 
-    def occupy(self, seat_id: str) -> None:
-        pass
+    def occupy(self) -> None:
+        connection = sqlite3.connect(self.database)
+        connection.execute("""
+        UPDATE "Seat" SET "taken" = 1 WHERE "seat_id" = ?
+        """, [self.seat_id])
+        connection.commit()
+        connection.close()
 
 
 class Card:
-    def __init__(self, type_card: str, number: str, cvc: str, holder: str,
+    def __init__(self, type_card: str, number: int, cvc: int, holder: str,
                  database: str = 'banking.db') -> None:
         self.type_card = type_card
         self.number = number
@@ -34,12 +46,38 @@ class Card:
         self.database = database
 
     def validate(self, price: float) -> bool:
-        pass
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""
+                    SELECT "type", "cvc", "holder", "balance" FROM 'Card' WHERE "number" == ?
+                    """, [self.number])
+            res = cursor.fetchall()[0]
+        except IndexError:
+            return False
+        connection.close()
+        return res[0] == self.type_card and res[1] == self.cvc \
+            and res[2] == self.holder and res[3] >= price
 
 
 class User:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def buy(self, seat: Seat, card: Card):
-        pass
+    @staticmethod
+    def buy(seat: Seat, card: Card):
+        seat.occupy()
+        connection = sqlite3.connect(card.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "balance" FROM "Card" WHERE "number" == ?
+        """, [card.number])
+        res = cursor.fetchall()[0][0]
+        bal = res - seat.price
+
+        connection.execute("""
+            UPDATE "Card" SET "balance" = ? WHERE "number" == ?
+        """, [bal, card.number])
+
+        connection.commit()
+        connection.close()

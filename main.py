f96e239d3cf824ef8db4084e168da1219c8860e7
rivka_yuliia <juliacreadora@gmail.com>
2023-02-07 16:14:12 +0200

    Refactor Ticket to buy method of User

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from purchase import User, Seat, Card
-from ticket import Ticket
 
 user = User(input('Your full name: '))
 seat = Seat(input('Preferred seat number: '))
@@ -12,9 +11,5 @@ else:
     card_holder = input('Card holder name: ')
 
     card = Card(card_type, card_number, card_cvc, card_holder)
-    if not card.validate(seat.price):
-        print("There's a problem with your card")
-    else:
-        user.buy(seat, card)
-        Ticket(user, seat).to_pdf()
-        print('Purchase successful!')
+
+    print(user.buy(seat, card))
--- purchase.py
+++ purchase.py
@@ -1,4 +1,7 @@
 import sqlite3
+from random import choice
+from string import ascii_letters, digits
+from fpdf import FPDF
 
 
 class Seat:
@@ -95,9 +98,13 @@ class User:
         Make the purchase of a ticket. Sets the seat to occupied and change
         the balance on the card
         """
+        if not card.validate(seat.price):
+            return "There's a problem with your card"
         seat.occupy()
         card_balance = self._get_card_balance(card)
         self._update_balance(seat, card, card_balance)
+        Ticket(self, seat).to_pdf()
+        return 'Purchase successful!'
 
     @staticmethod
     def _update_balance(seat: Seat, card: Card, card_balance: float) -> None:
@@ -127,3 +134,45 @@ class User:
         connection.commit()
         connection.close()
         return card_balance
+
+
+class Ticket:
+    """
+    Generates a Pdf file that contains the digital ticket that user has
+    purchased, with his name, number of the seat, id of the thicket and
+    the price.
+    """
+    filepath = 'ticket.pdf'
+
+    def __init__(self, user: User, seat: Seat):
+        self.id = ''.join([choice(ascii_letters + digits) for _ in range(8)])
+        self.user = user
+        self.seat = seat
+
+    def to_pdf(self):
+        """Generate and save the Pdf to the file"""
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add the title
+
+        pdf.set_font(family='Helvetica', size=32, style='B')
+        pdf.cell(w=0, h=80, txt="Your Digital Ticket", border=1, align='C', ln=1)
+
+        # Add the data of the user and the ticket
+
+        data = [('Name:', self.user.name), ('Ticket ID:', self.id),
+                ('Price:', self.seat.price), ('Seat number:', self.seat.seat_id)]
+
+        for label, value in data:
+            pdf.set_font(family='Helvetica', size=18, style='B')
+            pdf.cell(w=150, h=35, txt=label, border=1)
+            pdf.set_font(family='Helvetica', size=12)
+            pdf.cell(w=389, h=35, txt=f'{value}', border=1, ln=1)
+
+            pdf.cell(w=539, h=5, txt='', border=0, ln=1)
+
+        # Write the pdf file
+
+        pdf.output(self.filepath)
